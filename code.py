0602b1537c58b732304db72f5bc045a929331125
Dylan Mutabazi <dylan.mutabazi@stu.ocsb.ca>
2025-06-02 16:44:23 +0000

    Added sprite

--- code.py
+++ code.py
@@ -3,14 +3,19 @@ import stage
 
 def main():
     background_image = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    sprite_image = stage.Bank/from_bmp16("space_aliens.bmp")
+
     background = stage.Grid(background_image, 10, 8)
     
+    ship = stage.Sprite(sprite_image, 5, 75, 66)
+
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    game.layers = [ship] + [background]
     game.render_block()
 
     while True:
-        pass
+        game.render_sprites([ship])
+        game.tick
     
 if __name__ == "__main__":
     main()
