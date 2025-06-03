429c1b3c597ee2debd469f7f8cb0c4c289cc43c5
Dylan Mutabazi <dylan.mutabazi@stu.ocsb.ca>
2025-06-03 16:38:46 +0000

    Added movement

--- code.py
+++ code.py
@@ -26,10 +26,29 @@ def game_scene():
     game.render_block()
 
     while True:
-
         # redraws the sprites
         game.render_sprites([ship])
         game.tick()
+        
+        # user input
+        keys = ugame.buttons.get_pressed()
+        
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
     
 if __name__ == "__main__":
     game_scene()
