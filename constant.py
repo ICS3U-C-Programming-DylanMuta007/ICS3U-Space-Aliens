13f0390ecc023d9ad6ee34d8d4020c332d4290fd
Dylan Mutabazi <dylan.mutabazi@stu.ocsb.ca>
2025-06-10 16:20:08 +0000

    added title screen

--- constant.py
+++ constant.py
@@ -11,3 +11,13 @@ SPRITE_SIZE = 16
 TOTAL_NUMBER_OF_ALIENS = 10
 FPS = 30
 SPRITE_MOVEMENT_SPEED = 2
+
+# Using for button state
+button_state = {"button_up": "up",
+                "button_just_pressed": "just pressed",
+                "button_still_pressed": "still pressed",
+                "button_released": "released"
+                }
+
+RED_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+               b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
